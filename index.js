c7778d344d69f5ebf7becd8d869bf6920fc73181
Sean Elwood <seanelwood@gmail.com>
2017-11-07 03:50:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ function distanceFromHqInBlocks(pickUpLocation) {
     return pickUpLocation - 42;
   }
 }
+
+function distanceFromHqInFeet(pickUpLocation) {
+  //Returns the number of blocks from Scuber's headquarters to the pickup location. Note that you can use your distanceFromHqInBlocks function to help return the correct value here.
+  return distanceFromHqInBlocks(pickUpLocation) * 264;
+}
