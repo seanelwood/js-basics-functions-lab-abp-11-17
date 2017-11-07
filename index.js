6aeb0da5f9618a9f329802fb9946718a8db10d3b
Sean Elwood <seanelwood@gmail.com>
2017-11-07 03:54:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function distanceFromHqInFeet(pickUpLocation) {
 
 function distanceTravelledInFeet(pickUpLocation, endingBlock) {
   //Calculates the number of feet a passenger travels given a starting block and an ending block - it only calculates distance North and South. It uses the knowledge that a block is 264 feet long.
-
-}
+    return (pickUpLocation - endingBlock) * 264;
+  }
